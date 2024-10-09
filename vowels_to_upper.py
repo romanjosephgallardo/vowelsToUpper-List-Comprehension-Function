aaa971dceac096533cd4a3d1b30a174bbf685fa1
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 19:32:14 +0800

    Remove docstrings and fix comments for readability.

--- vowels_to_upper.py
+++ vowels_to_upper.py
@@ -1,43 +1,23 @@
-'''
-## Lab exercise 3
-### Provide a list comprehension that implementation for a function called vowelsToUpper with the following signature:
+# Vowels to Upper
 
-method name : vowelsToUpper
-
-input argument : String 
-
-return argument : String
-
-vowelsToUpper must return a version of its String argument with all its vowels changed to their uppercase forms. Nonvowel characters stay as is.
-
-### Sample Calls
-
-vowelsToUpper "" == ""
-
-vowelsToUpper "Hello, world!" == "HEllO, wOrld!"
-
-vowelsToUpper "hello hi bye" == "hEllO hI byE"
-'''
-
-# pseudocode
-
-# define a class called vowelsToUpper that takes a string as input
+# Define a class called vowelsToUpper that takes a string as input
 class VowelsToUpper:
     def __init__(self, string):
         self.string = string
-    # define a function called vowelsToUpper that takes a string as input
+    # Define a function called vowelsToUpper that takes a string as input
     def vowelsToUpper(self):
         lower_case_vowels = ['a', 'e', 'i', 'o', 'u']
+        # Use list comprehension to change the vowels to uppercase
         result = ''.join([character.upper() if character in lower_case_vowels else character for character in self.string])
         return result
     
-# make a user input for the string
+# Make a user input for the string
 user_input_string = input("Enter a string: ")
 
-# instantiate the class and call the method
+# Instantiate the class and call the method
 vowels_to_upper = VowelsToUpper(user_input_string)
 result = vowels_to_upper.vowelsToUpper()
 
-# print the result
+# Print the result
 print(result)
 
