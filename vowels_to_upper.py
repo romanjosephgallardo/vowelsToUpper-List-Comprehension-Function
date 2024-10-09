0241a15a55c9efbc9e80b822e22df3c9a90f7f43
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-09 17:58:59 +0800

    Add pseudocode.

---
+++ vowels_to_upper.py
@@ -0,0 +1,35 @@
+'''
+## Lab exercise 3
+### Provide a list comprehension that implementation for a function called vowelsToUpper with the following signature:
+
+method name : vowelsToUpper
+
+input argument : String 
+
+return argument : String
+
+vowelsToUpper must return a version of its String argument with all its vowels changed to their uppercase forms. Nonvowel characters stay as is.
+
+### Sample Calls
+
+vowelsToUpper "" == ""
+
+vowelsToUpper "Hello, world!" == "HEllO, wOrld!"
+
+vowelsToUpper "hello hi bye" == "hEllO hI byE"
+'''
+
+# pseudocode
+# define a class called vowelsToUpper that takes a string as input
+    # define a function called vowelsToUpper that takes a string as input
+        # create a list of vowels
+        # create a list of uppercase vowels
+        # convert the string to a list of characters
+        # create a list comprehension that checks if the character is a vowel
+    # return a string with all vowels changed to their uppercase forms
+
+# make a user input for the string
+# instantiate the class and call the method
+# print the result
+
+
